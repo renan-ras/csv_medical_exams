3460b28b3252c8b2ba7956ba54867647b62efa27
Renan Antonio da Silva <renanras@gmail.com>
2023-07-24 20:47:06 -0300

    HTML para busca por token; Falta add +info

--- init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Execute migrations
-rake db:migrate
-
-# Start the server
-ruby server.rb
--- public/main.js
+++ public/main.js
@@ -1,39 +1,58 @@
-const url = 'http://localhost:3000/tests';
+const searchForm = document.getElementById('search-form');
+const searchTokenInput = document.getElementById('search-token');
+const showAllButton = document.getElementById('show-all');
 
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    const tableBody = document.querySelector('#exam-table tbody');
-    tableBody.innerHTML = '';
+function fetchExams(url) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const tableBody = document.querySelector('#exam-table tbody');
+      tableBody.innerHTML = '';
 
-    data.forEach((exam) => {
-      const row = document.createElement('tr');
+      data.forEach((exam) => {
+        const row = document.createElement('tr');
 
-      const idCell = document.createElement('td');
-      idCell.textContent = exam.id;
-      row.appendChild(idCell);
+        const idCell = document.createElement('td');
+        idCell.textContent = exam.id;
+        row.appendChild(idCell);
 
-      const cpfCell = document.createElement('td');
-      cpfCell.textContent = exam.cpf;
-      row.appendChild(cpfCell);
+        const cpfCell = document.createElement('td');
+        cpfCell.textContent = exam.cpf;
+        row.appendChild(cpfCell);
 
-      const typeCell = document.createElement('td');
-      typeCell.textContent = exam.exam_type;
-      row.appendChild(typeCell);
+        const typeCell = document.createElement('td');
+        typeCell.textContent = exam.exam_type;
+        row.appendChild(typeCell);
 
-      const limitsCell = document.createElement('td');
-      limitsCell.textContent = exam.exam_limits;
-      row.appendChild(limitsCell);
+        const limitsCell = document.createElement('td');
+        limitsCell.textContent = exam.exam_limits;
+        row.appendChild(limitsCell);
 
-      const resultCell = document.createElement('td');
-      resultCell.textContent = exam.exam_result;
-      row.appendChild(resultCell);
+        const resultCell = document.createElement('td');
+        resultCell.textContent = exam.exam_result;
+        row.appendChild(resultCell);
 
-      const tokenCell = document.createElement('td');
-      tokenCell.textContent = exam.exam_token;
-      row.appendChild(tokenCell);
+        const tokenCell = document.createElement('td');
+        tokenCell.textContent = exam.exam_token;
+        row.appendChild(tokenCell);
 
-      tableBody.appendChild(row);
-    });
-  })
-  .catch((error) => console.log(error));
+        tableBody.appendChild(row);
+      });
+    })
+    .catch((error) => console.log(error));
+}
+
+searchForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const token = searchTokenInput.value;
+  const url = `http://localhost:3000/tests/${token}`;
+
+  fetchExams(url);
+});
+
+showAllButton.addEventListener('click', function() {
+  const url = 'http://localhost:3000/tests';
+
+  fetchExams(url);
+});
